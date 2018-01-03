19a8cb891043fc82249c03bd7a77d4c7d9b2c5f0
Patrick Thomas <pwt5ca@virginia.edu>
2018-01-03 00:17:15 +0000

    Now hooks in with SPI stuff

--- main.py
+++ main.py
@@ -1,36 +1,51 @@
 #!/usr/bin/python3
 
-# import RPi.GPIO as GPIO
+import spidev
 
 import remi.gui as gui
 from remi import start, App
 from threading import Timer
 
 # some constants
-V_OUT_PIN = 11
 PORT = 8081
 ADDRESS = "10.48.125.105"
 
-# GPIO.setmode(GPIO.BCM)  # set board mode to Broadcom
-# GPIO.setup(V_OUT_PIN, GPIO.IN)
+# SPI variables
+DELAY = 0.1
+LDR_CHANNEL = 0
+
+# create SPI
+spi = spidev.SpiDev()
+spi.open(0, 0)
+
+
+def readADC(adc_num):
+    # read spi data from the MCP3008, 8 channels in total
+    if (adc_num > 7) or (adc_num < 0):
+        return -1
+
+    r = spi.xfer2([1, 8+adc_num << 4, 0])
+    data = ((r[1] & 3) << 8) + r[2]
+    return data
+
 
 class TemperatureApp(App):
     def __init__(self, *args):
         super(TemperatureApp, self).__init__(*args)
 
     def main(self):
-        self.count = 0
+        # create label that will display the info
         self.lbl = gui.Label("Hello world!", width=100, height=30)
 
+        # call the display temperature function
         self.display_temperature()
 
         # return the root of the widget
         return self.lbl
 
     def display_temperature(self):
-        self.lbl.set_text(str(self.count))
-        self.count += 1
-        Timer(1, self.display_temperature).start()
+        self.lbl.set_text(str(readADC(LDR_CHANNEL))
+        Timer(DELAY, self.display_temperature).start()
 
 start(
     TemperatureApp, 
@@ -38,6 +53,6 @@ start(
     port=PORT, 
     multiple_instance=False,
     enable_file_cache=True,
-    update_interval=0.1, 
+    update_interval=DELAY, 
     start_browser=False
 )
